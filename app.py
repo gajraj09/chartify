6d04d1ae95123c9a6979acbfb3c8ae8231fec64a
gajraj09 <153438323+gajraj09@users.noreply.github.com>
2025-09-11 12:39:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ LENGTH = 3  # number of LAST CLOSED candles to compute bounds
 # LENGTH = 2  # number of LAST CLOSED candles to compute bounds
 
 MONGO_URI = os.environ.get("MONGO_URI", "mongodb://localhost:27017")
-DB_NAME = "trading_bot_longonly"
+DB_NAME = "trading_bot_eth_longonly"
 COLLECTION_STATE = "bot_state"
 
 # ==========================
